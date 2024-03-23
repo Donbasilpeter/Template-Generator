695e0596a36219e844c21e7ec1475a066f85a1d0
Don Basil Peter <donbasilpeter@gmail.com>
2024-03-23 16:07:52 -0400

    updated the ui

---
+++ frontend/src/Components/loading.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+
+const Loading = () => {
+
+  return (
+    <Box
+      display="flex"
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="center"
+      minHeight="100%"
+
+    >
+      <Typography variant="h3" gutterBottom>
+        Loading
+      </Typography>
+    </Box>
+  );
+};
+
+export default Loading;
