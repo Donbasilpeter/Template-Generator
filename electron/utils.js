26e77c558be00664c05942435226fc0414b471c5
Don Basil Peter <donbasilpeter@gmail.com>
2024-07-04 21:16:26 -0400

    added flag to skip installation after initial setup

--- electron/utils.js
+++ electron/utils.js
@@ -1,31 +1,44 @@
 const path = require('path');
 const fs = require('fs-extra');
-const { exec,spawn } = require('child_process');
+const { exec, spawn } = require('child_process');
 const util = require('util');
 const execPromise = util.promisify(exec);
 
-async function extractApp(app) {
+async function extractApp(reactAppPath) {
   const appPath = path.join(__dirname, '../demo-app');
-  const writablePath = path.join(app.getPath('userData'), 'demo-app');
-  if (fs.existsSync(writablePath)) {
-    await fs.remove(writablePath);
+  if (fs.existsSync(reactAppPath)) {
+    await fs.remove(reactAppPath);
   }
-  await fs.copy(appPath, writablePath);
-  return writablePath;
+  await fs.copy(appPath, reactAppPath);
 }
 
 async function installDependencies(reactAppPath) {
   try {
     const installResult = await execPromise('npm install', { cwd: reactAppPath });
     console.log(`React app install stdout: ${installResult.stdout}`);
-    // Start the React app after installation
     if (installResult.stderr) {
       console.error(`React app install stderr: ${installResult.stderr}`);
     }
-    // Start the React app after installation
-    spawn('node', ['esbuild.config.js'], { cwd: reactAppPath });
   } catch (error) {
     console.error(`Error installing React app dependencies: ${error}`);
+    throw error;
+  }
+}
+
+ function runBuild(reactAppPath) {
+  try {
+    const buildProcess = spawn('node', ['esbuild.config.js'], { cwd: reactAppPath });
+    buildProcess.stdout.on('data', (data) => {
+      console.log(`Build stdout: ${data}`);
+    });
+    buildProcess.stderr.on('data', (data) => {
+      console.error(`Build stderr: ${data}`);
+    });
+    buildProcess.on('close', (code) => {
+      console.log(`Build process exited with code ${code}`);
+    });
+  } catch (error) {
+    console.error(`Error running build script: ${error}`);
     throw error;
   }
 }
@@ -53,5 +66,6 @@ function createStructure(basePath, structure) {
 module.exports = {
   extractApp,
   installDependencies,
+  runBuild,
   createStructure
 };
